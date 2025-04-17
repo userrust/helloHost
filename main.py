0ed255a575cd1f1d62c216bcf20fa83d050f016c
Андрей <andrei.red456@gmail.com>
2025-04-17 23:49:08 +0300

    Добавлен

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ app = FastAPI()
 # Монтируем статические файлы (HTML/CSS/JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
 async def read_root():
-    return {"message": "Добро пожаловать в FastBank API!"}
+    return {"message":"Добро пожаловать в FastBank API!1"}
+
 
 @app.get("/api/balance")
 async def get_balance():
-    return {"balance": 10000, "currency": "RUB"}
+    return {"balance":10000, "currency":"RUB"}
+
 
 @app.post("/api/transfer")
 async def transfer_money(amount: float, recipient: str):
     # Здесь была бы логика перевода
-    return {"status": "success", "message": f"Переведено {amount} RUB пользователю {recipient}"}
+    return {"status":"success", "message":f"Переведено {amount} RUB пользователю {recipient}"}
