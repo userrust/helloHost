26ea063b1d84e891cb0ec057af3adcf7ef5f4990
Андрей <andrei.red456@gmail.com>
2025-04-18 14:00:56 +0300

    Добавлен

--- main.py
+++ main.py
@@ -9,15 +9,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def read_root():
-    return {"message":"Добро пожаловать в FastBank API!11"}
+    return {"message": "Добро пожаловать в FastBank API!11"}
 
 
-@app.get("/api/balance")
-async def get_balance():
-    return {"balance":10000, "currency":"RUB"}
-
-
-@app.post("/api/transfer")
-async def transfer_money(amount: float, recipient: str):
+@app.post("/api")
+async def transfer_money():
     # Здесь была бы логика перевода
-    return {"status":"success", "message":f"Переведено {amount} RUB пользователю {recipient}"}
+    return {"status":"success"}
